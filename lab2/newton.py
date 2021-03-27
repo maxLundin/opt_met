e109eeff2ded55b152a8e1a3b01a338cf77f0c5c
friendlyevil <FriendlyEvil@yandex.ru>
2021-03-27 13:27:52 +0300

    add lab2 ipynd

--- lab2/newton.py
+++ lab2/newton.py
@@ -5,6 +5,7 @@ import functions
 def is_pos_def(x):
     return np.all(np.linalg.eigvals(x) > 0)
 
+
 def make_pos_def(x):
     l = np.linalg.eigvals(x)
     mu = (-l + 1) * np.eye(x.shape[0])
@@ -36,4 +37,3 @@ def newton(f, f_grad, f_hess, x0, eps=1e-6, max_iter=1e6):
 def analyze(f, x_border, y_border, f_grad, f_hess, x0):
     return functions.analyze(f, x_border, y_border, lambda: newton(f, f_grad, f_hess, x0))
 
-# analyze(functions.f3, (-30, 30), (-30, 30), functions.f3_grad, functions.f3_hess, (-3, 3))
